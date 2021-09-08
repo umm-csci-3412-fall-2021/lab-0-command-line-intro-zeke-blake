ddd227ad5c64cbb68df3e441d9c9ec735155494a
brwjxxvi <77683978+brwjxxvi@users.noreply.github.com>
2021-09-07 22:15:02 -0500

    Final big_clean.sh update
    Added comments to every command in order to allow for easier reading and understanding of the script.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Stores the given argument directory .tgz
 fname=$(basename "$1" .tgz)
 
+#Stores the given argument directory with "cleaned_" prefix
 zname="cleaned_${fname}"
 
 #Stores the temporary directory
@@ -10,15 +12,20 @@ TMPDIR=$(mktemp -d)
 #Stores the current working directory (in this case ../cleaning)
 here=$(pwd)
 
+#Uncompresses and extracts contents of given argument directory in temporary directory
 tar -zxf "$1" -C "$TMPDIR"
 
+#Routes to unzipped/uncompressed argument directory in temporary directory
 cd "$TMPDIR"/"$fname" || exit
 
+#Finds and removes all files in uncompressed argument directory with "DELETE ME!" line
 grep -lr "DELETE ME!" | xargs rm
 
+#Routes to previous directory
 cd ..
 
+#Recompresses new argument directory with "cleaned_" prefix and .tgz suffix
 tar -czf "$zname".tgz "$fname"
 
-#Moves the .tgz file to the stored directory
+#Moves the .tgz file to the stored directory (../cleaning)
 mv "$zname".tgz "$here"
